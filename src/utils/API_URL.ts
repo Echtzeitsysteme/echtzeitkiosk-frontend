517be380ab8864608240818852fdb0fba32b0809
Mertalp Ulus <mertalp.ulus@gmail.com>
2022-08-22 16:25:14 +0200

    fix(api url): quick fix for deployment

--- src/utils/API_URL.ts
+++ src/utils/API_URL.ts
@@ -1,4 +1,7 @@
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://api.echtzeitkiosk.mertalpulus.eu/v1"
-    : "http://localhost:4000/v1";
+// export const API_URL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REACT_APP_API_URL
+//     : "http://localhost:4000/v1";
+
+export const API_URL = process.env.REACT_APP_API_URL;
+// export const API_URL = "http://localhost:4000/v1";
