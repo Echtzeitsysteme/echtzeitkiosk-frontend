eb6588e607dca992e7d0baf36586e045c1407f99
Mertalp Ulus <mertalp.ulus@gmail.com>
2022-09-12 03:37:46 +0200

    :sparkles: (SystemState) create new page for the superuser role

--- src/pages/superuserPages/SystemState/index.tsx
+++ src/pages/superuserPages/SystemState/index.tsx
@@ -1,37 +1,36 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-import { DataGrid } from "@mui/x-data-grid";
 import axios from "axios";
-import moment from "moment";
+
 import Swal from "sweetalert2";
 
-import {
-  Form,
-  useTranslate,
-  useNotify,
-  required,
-  TextInput,
-  email,
-} from "react-admin";
+import { useTranslate } from "react-admin";
 
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CircularProgress,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Card, Typography } from "@mui/material";
 
 import { API_URL } from "../../../utils/API_URL";
 
 const SystemState = () => {
-  const [loading, setLoading] = useState(true);
+  const [systemBalance, setSystemBalance] = useState(undefined);
 
   const translate = useTranslate();
-  const notify = useNotify();
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization: `${localStorage.getItem("token")}`,
+      },
+      url: `${API_URL}/system-state/balance`,
+    };
+
+    axios(options).then((response) => {
+      console.log(response.data);
+      setSystemBalance(response.data.data.balance);
+    });
+  }, [systemBalance]);
 
   return (
     <Box
@@ -50,149 +49,179 @@ const SystemState = () => {
       <Card
         sx={{
           display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-evenly",
+          flexDirection: "column",
+          
           flexWrap: "wrap",
           alignItems: "center",
           minWidth: "95vw",
           minHeight: "80vh",
           marginTop: "1em",
+          paddingTop: "1em",
           boxShadow: "0px 10px 13px -7px #000000",
           border: "5px 5px 15px 5px #000000",
         }}
       >
-        <Card
+        <Box>
+          <Typography variant="h4" sx={{ color: "primary" }}>
+            {/* {translate("resources.systemState.name")} */}
+            System State
+          </Typography>
+        </Box>
+        <Box
           sx={{
-            minWidth: "20vw",
-            minHeight: "20vh",
-            boxShadow: "0px 10px 13px -7px #000000",
-            border: "5px 5px 15px 5px #000000",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            flexWrap: "wrap",
+            alignItems: "center",
           }}
         >
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ margin: "1em" }}
-            onClick={() => {
-              setLoading(true);
-              axios
-                .get(`${API_URL}/systemState`)
-                .then((res) => {
-                  console.log(res.data);
-                  setLoading(false);
-                })
-                .catch((err) => {
-                  console.log(err);
-                  setLoading(false);
-                });
+          <Card
+            sx={{
+              boxShadow: "0px 10px 13px -7px #000000",
+              border: "5px 5px 15px 5px #000000",
+              margin: "1rem",
             }}
           >
-            Send Invoice To All Customers
-          </Button>
-        </Card>
-
-        <Card
-          sx={{
-            minWidth: "20vw",
-            minHeight: "20vh",
-            boxShadow: "0px 10px 13px -7px #000000",
-            border: "5px 5px 15px 5px #000000",
-          }}
-        >
-          <Typography>Current Cash Balance: ...</Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ margin: "1em" }}
-            onClick={async () => {
-              await Swal.fire({
-                title: translate("echtzeitkiosk.balance.update"),
-                text: translate("echtzeitkiosk.balance.update_descr"),
-                input: "text",
-                inputPlaceholder: translate(
-                  "echtzeitkiosk.balance.update_placeholder"
-                ),
-                showCancelButton: true,
-                cancelButtonText: translate("echtzeitkiosk.buttons.cancel"),
-                confirmButtonText: translate("echtzeitkiosk.buttons.update"),
-                showLoaderOnConfirm: true,
-                preConfirm: async (balance) => {
-                  try {
-                    const urlParams = {
-                      balance,
-                    };
-                    const data = Object.keys(urlParams)
-                      .map(
-                        (key) => `${key}=${encodeURIComponent(urlParams[key])}`
-                      )
-                      .join("&");
-
+            <Button
+              variant="contained"
+              color="warning"
+              sx={{ margin: "1em" }}
+              onClick={() => {
+                Swal.fire({
+                  title: "Are you sure?",
+                  text: "You won't be able to revert this!",
+                  icon: "warning",
+                  showCancelButton: true,
+                  confirmButtonColor: "#3085d6",
+                  cancelButtonColor: "#d33",
+                  confirmButtonText: "Yes, send them!",
+                }).then((result) => {
+                  if (result.isConfirmed) {
                     const options = {
-                      method: "PATCH",
+                      method: "POST",
                       headers: {
-                        "content-type": "application/x-www-form-urlencoded",
                         Authorization: `${localStorage.getItem("token")}`,
                       },
-
-                      data,
-                      url: `${API_URL}/system-state/balance/`,
+                      url: `${API_URL}/customer-invoices/send-everyone`,
                     };
+                    axios
+                      .request(options)
+                      .then((res) => {
+                        if (res.status === 200) {
+                          Swal.fire({
+                            title: "Success",
+                            text: "Invoices sent successfully",
+                            icon: "success",
+                            confirmButtonText: "Ok",
+                          });
+                        }
+                      })
+                      .catch((err) => {
+                        console.log(err);
+                      });
+                  }
+                });
+              }}
+            >
+              Send Invoice To All Customers
+            </Button>
+          </Card>
 
-                    const resp = await axios(options);
-                  } catch (error) {}
-                },
-                allowOutsideClick: () => !Swal.isLoading(),
-              }).finally(() => {
-                window.location.reload();
-              });
+          <Card
+            sx={{
+              // minWidth: "15vw",
+              // minHeight: "15vh",
+              margin: "1rem",
+              padding: "1em",
+              boxShadow: "0px 10px 13px -7px #000000",
+              border: "5px 5px 15px 5px #000000",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            Update Cash Balance
-          </Button>
-        </Card>
+            <Typography>
+              {translate("echtzeitkiosk.treasury.current_balance")}:{" "}
+              {systemBalance !== undefined
+                ? `${systemBalance} €`
+                : "Loading..."}
+            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ margin: "1em" }}
+              onClick={async () => {
+                await Swal.fire({
+                  title: translate(
+                    "echtzeitkiosk.treasury.update_dialog.title"
+                  ),
+                  text: translate(
+                    "echtzeitkiosk.treasury.update_dialog.description"
+                  ),
+                  input: "text",
+                  inputPlaceholder: translate(
+                    "echtzeitkiosk.treasury.update_dialog.placeholder"
+                  ),
+                  showCancelButton: true,
+                  cancelButtonText: translate("echtzeitkiosk.buttons.cancel"),
+                  confirmButtonText: translate("echtzeitkiosk.buttons.update"),
+                  showLoaderOnConfirm: true,
+                  backdrop: true,
+                  preConfirm: async (balance) => {
+                    try {
+                      const urlParams = {
+                        balance,
+                      };
+                      const data = Object.keys(urlParams)
+                        .map(
+                          (key) =>
+                            `${key}=${encodeURIComponent(urlParams[key])}`
+                        )
+                        .join("&");
 
-        <Card
-          sx={{
-            minWidth: "20vw",
-            minHeight: "20vh",
-            boxShadow: "0px 10px 13px -7px #000000",
-            border: "5px 5px 15px 5px #000000",
-          }}
-        >
-          <Typography>Test</Typography>
-        </Card>
-        <Card
-          sx={{
-            minWidth: "20vw",
-            minHeight: "20vh",
-            boxShadow: "0px 10px 13px -7px #000000",
-            border: "5px 5px 15px 5px #000000",
-          }}
-        >
-          <Typography>Test</Typography>
-        </Card>
+                      const options = {
+                        method: "PATCH",
+                        headers: {
+                          "content-type": "application/x-www-form-urlencoded",
+                          Authorization: `${localStorage.getItem("token")}`,
+                        },
 
-        <Card
-          sx={{
-            minWidth: "20vw",
-            minHeight: "20vh",
-            boxShadow: "0px 10px 13px -7px #000000",
-            border: "5px 5px 15px 5px #000000",
-          }}
-        >
-          <Typography>Test</Typography>
-        </Card>
+                        data,
+                        url: `${API_URL}/system-state/balance`,
+                      };
 
-        <Card
-          sx={{
-            minWidth: "20vw",
-            minHeight: "20vh",
-            boxShadow: "0px 10px 13px -7px #000000",
-            border: "5px 5px 15px 5px #000000",
-          }}
-        >
-          <Typography>Test</Typography>
-        </Card>
+                      const resp = await axios(options);
+
+                      if (resp.status === 200) {
+                        setSystemBalance(resp.data.data.balance);
+                        return true;
+                      }
+                    } catch (error) {}
+                  },
+                  allowOutsideClick: () => !Swal.isLoading(),
+                }).finally(() => {
+                  // window.location.reload();
+                });
+              }}
+            >
+              {translate("echtzeitkiosk.treasury.update_dialog.title")}
+            </Button>
+          </Card>
+
+          <Card
+            sx={{
+              minWidth: "20vw",
+              minHeight: "20vh",
+              margin: "1rem",
+              boxShadow: "0px 10px 13px -7px #000000",
+              border: "5px 5px 15px 5px #000000",
+            }}
+          >
+            <Typography>Test</Typography>
+          </Card>
+        </Box>
       </Card>
     </Box>
   );
